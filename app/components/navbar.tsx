2670b4d31866b5e07006f672dce88eec26640022
Jakob Ossmann <Jakobossmann@gmail.com>
2024-05-21 23:15:10 +1000

    navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation'
+
+export default function Navbar() {
+  const pathname = usePathname()
+  const links = [
+    { href: '/placement', label: 'My WIL' },
+    { href: '/skills', label: 'My Technical Skills' },
+    { href: '/ethics', label: 'My Ethics' },
+    { href: '/blog', label: 'My Extra-Curricular' },
+    { href: '/faq', label: 'About Me' },
+  ];
+
+  return (
+    <div>
+      <div className='flex justify-between p-4 bg-white border font-light text-sm px-20'>
+        <div className='flex justify-start hover:underline pl-2'>
+          <Link href="/">
+            Home
+          </Link>
+        </div>
+  
+        <div className='flex justify-center'>
+          {links.map((link, index) => (
+            <div className='px-4 hover:underline' key={index}>
+              <Link className={`link ${pathname === link.href ? 'font-semibold' : ''}`} href={link.href}>
+                {link.label}
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
