0269969f867fb407778dfbc74417dc7835893106
Jakob Ossmann <Jakobossmann@gmail.com>
2024-08-07 22:35:15 +1000

    horizontal scroller

--- app/components/FightCardsScroller.tsx
+++ app/components/FightCardsScroller.tsx
@@ -1,18 +1,28 @@
 
 import { getFightCards } from '../data/data';
 import { FightCard } from '../data/definitions';
+import React, { useState } from 'react';
+
 
 export default async function FightCardsScroller() {
     const fightCards = await getFightCards();
 
     return (
-        <div className='overflow-x-auto whitespace-nowrap'>
-            {fightCards.map((fightCard) => (
-                <div className='inline-block px-4 py-2' key={fightCard.fight_card_id}>
-                    {fightCard.name}
-                </div>
-            ))}
-        </div>
+        <div className="scroll-container">
+        {fightCards.map((fightCard) => (
+          <div className="scroll-item" key={fightCard.fight_card_id}>
+            {fightCard.name}
+          </div>
+        ))}
+        <div className='scroll-item'>test1</div>
+        <div className='scroll-item'>test2</div>
+        <div className='scroll-item'>test2</div>
+        <div className='scroll-item'>test2</div>
+        <div className='scroll-item'>test2</div>
+        <div className='scroll-item'>test2</div>
+        <div className='scroll-item'>test2</div>
+        <div className='scroll-item'>test2</div>
+      </div>      
     );
 }
 
